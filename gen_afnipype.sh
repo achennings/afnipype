4da4cc18a90f4977044896e3b07217472f5bc56e
achennings <achennings@utexas.edu>
2021-04-28 11:19:19 -0500

    full env now

--- gen_afnipype.sh
+++ gen_afnipype.sh
@@ -8,12 +8,18 @@ generate_docker() {
     --install vim python \
     --user=root \
     --afni version=latest install_r=TRUE install_r_pkgs=TRUE method=binaries \
-    --fsl version=5.0.10 method=binaries \
+    --fsl version=6.0.4 method=binaries \
+    --ants version=2.3.1 method=binaries \
+    --convert3d version=1.0.0 method=binaries \
+    --freesurfer version=7.1.1 method=binaries \
+    --copy license.txt /license.txt \
+    --env FS_LICENSE=/license.txt \
+    --matlabmcr version=2018a method=binaries \
     --user=neuro \
     --miniconda \
-          conda_install='python=3.7 matplotlib numpy pandas scikit-learn nilearn scipy seaborn traits' \
+          conda_install='python=3.8 matplotlib numpy pandas scikit-learn nilearn scipy seaborn traits' \
           pip_install='nibabel nipype pingouin ipython' \
-          create_env="neuro_py36" \
+          create_env="neuro" \
           activate=true
 }
 
@@ -23,12 +29,18 @@ generate_singularity() {
     --install vim python \
     --user=root \
     --afni version=latest install_r=TRUE install_r_pkgs=TRUE method=binaries \
-    --fsl version=5.0.10 method=binaries \
+    --fsl version=6.0.4 method=binaries \
+    --ants version=2.3.1 method=binaries \
+    --convert3d version=1.0.0 method=binaries \
+    --freesurfer version=7.1.1 method=binaries \
+    --copy license.txt /license.txt \
+    --env FS_LICENSE=/license.txt \
+    --matlabmcr version=2018a method=binaries \
     --user=neuro \
     --miniconda \
-          conda_install='python=3.7 matplotlib numpy pandas scikit-learn nilearn scipy seaborn traits' \
+          conda_install='python=3.8 matplotlib numpy pandas scikit-learn nilearn scipy seaborn traits' \
           pip_install='nibabel nipype pingouin ipython' \
-          create_env="neuro_py36" \
+          create_env="neuro" \
           activate=true
 }
 
